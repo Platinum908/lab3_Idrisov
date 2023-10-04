3ee07c1730d19bb3c09b60ce11563335f5054637
Administrator <Administrator@WIN-QH3366O2A51>
2023-10-04 18:47:39 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿//// начинается с пн
+//Console.Write("k: ");
+//int k = int.Parse(Console.ReadLine());
+//int n = (k + 0) % 7;
+//Console.WriteLine($"{k} день года, является {n} днём недели");
+
+//// начинается с вторника
+//Console.Write("k: ");
+//int k = int.Parse(Console.ReadLine());
+//int n = (k + 1) % 7;
+//Console.WriteLine($"{k} день года, является {n} днём недели");
+
+//// начинается d-дня недели
+//Console.Write("k: ");
+//int k = int.Parse(Console.ReadLine());
+//Console.Write("d: ");
+//int d = int.Parse(Console.ReadLine());
+//int n = (k + (d - 1)) % 7;
+//Console.WriteLine($"{k} день года, является {n} днём недели");
